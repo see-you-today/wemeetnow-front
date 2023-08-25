2c9c3badbdc9173eeb0b1e6e89c234529c69079f
DongHyeonWon <dhe7700@naver.com>
2023-08-25 23:44:25 +0900

    rename: 컴포넌트 이름 변경

--- src/screens/ChatRoom.tsx
+++ src/screens/ChatRoom.tsx
@@ -1,30 +1,6 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { theme } from "../utils/themes";
-import { checkChatRooms } from "../apis/chatroom";
+import React from "react";
+import { Text } from "react-native";
 
 export default function ChatRoom() {
-  useEffect(() => {
-    checkChatRooms().then((res) => {
-      console.log(res.data);
-    });
-  }, []);
-  return (
-    <View style={styles.container}>
-      <Text>dfd</Text>
-    </View>
-  );
+  return <Text>ChatRoom</Text>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    height: "100%",
-    flexDirection: "row",
-    backgroundColor: theme.color.backGround,
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: "bold",
-    marginLeft: 18,
-  },
-});
